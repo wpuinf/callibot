a63d4f3978af927c4b58af53e3a6de29e31be88c
Jimi <ib1jimbob@gmail.com>
2022-02-28 08:50:04 +0100

    Defined the drive, left and right functions

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
+basic.forever(function () {
+	
+})
 
+
+function drive(speed: number, time: number):void {
+    if (speed > 0 && speed < 100) {
+        calliBot2.motor(C2Motor.beide, C2Dir.vorwärts, speed);
+    } else if (speed < 0 && speed < -100) {
+        calliBot2.motor(C2Motor.beide, C2Dir.rückwärts, speed);
+    } else {
+        basic.showIcon(IconNames.No);
+        return;
+    }
+    basic.pause(time);
+    calliBot2.motorStop(C2Motor.beide, C2Stop.Frei)
+}
+
+function left(angle: number): void {
+    calliBot2.motor(C2Motor.links, C2Dir.rückwärts, 50);
+    calliBot2.motor(C2Motor.rechts, C2Dir.vorwärts, 50);
+    basic.pause(angle * 5);
+    calliBot2.motorStop(C2Motor.beide, C2Stop.Frei);
+}
+
+function right(angle: number): void {
+    calliBot2.motor(C2Motor.links, C2Dir.vorwärts, 50);
+    calliBot2.motor(C2Motor.rechts, C2Dir.rückwärts, 50);
+    basic.pause(angle * 5);
+    calliBot2.motorStop(C2Motor.beide, C2Stop.Frei);
+} 
